e7ce55c71bf55cd147a31cb30b435341fd84db4a
ulxsth <ulxsth@gmail.com>
2025-09-05 17:24:54 +0900

    chore: add use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState, useEffect } from 'react';
 import { TitleScreen } from '../components/TitleScreen';
 import { NameInputScreen } from '../components/NameInputScreen';
