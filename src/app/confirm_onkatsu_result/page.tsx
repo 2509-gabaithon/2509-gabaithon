4f4e726973090a54d22d08acc6fc3492bf2d5bc5
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2025-09-06 15:25:44 +0900

    fix: 引数名の修正

--- src/app/confirm_onkatsu_result/page.tsx
+++ src/app/confirm_onkatsu_result/page.tsx
@@ -2,12 +2,14 @@
 
 import { ResultScreen } from "@/components/ResultScreen";
 import onsenStamp from "@/assets/23d72f267674d7a86e5a4d3966ba367d52634bd9.png";
+import characterImage from '@/assets/ac6d9ab22063d00cb690b5d70df3dad88375e1a0.png';
 
 export default function ConfirmOnkatsuResult() {
   const expGained = 50;
   const levelUp = false;
   const character = {
     name: "もちもちうさぎ",
+    image: characterImage.src,
     level: 5,
     exp: 150,
     maxExp: 200,
@@ -18,7 +20,7 @@ export default function ConfirmOnkatsuResult() {
     icon: onsenStamp.src,
   };
 
-  const handleContinue = () => {
+  const handleNavigateToDecoration = () => {
     console.log("キャラクター画面へ移動");
   };
 
@@ -28,7 +30,7 @@ export default function ConfirmOnkatsuResult() {
       levelUp={levelUp}
       character={character}
       acquiredStamp={acquiredStamp}
-      onContinue={handleContinue}
+      onNavigateToDecoration={handleNavigateToDecoration}
     />
   );
 }
