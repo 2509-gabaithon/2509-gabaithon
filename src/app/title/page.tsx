1dd25f4aada3930f0be82a1fd979142efa426cbe
58140-web <s58140@ga.ariake-nct.ac.jp>
2025-09-07 22:01:12 +0900

    Add: サウンド再生用のユーティリティ関数playSoundを追加し、タイトル画面でクリック音を再生するように実装

--- src/app/title/page.tsx
+++ src/app/title/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { TitleScreen } from "@/components/TitleScreen";
+import { playSound } from "@/utils/playSound";
 
 export default function Title() {
-   const clickSound = new Audio("/clickSound.mp3");
 
   const handleStart = () => {
-    clickSound.play();
+    playSound("clickSound.mp3");
     console.log("タイトル画面クリック → ゲーム開始！");
   };
 
   const handleSettings = () => {
+    playSound("clickSound.mp3");
     console.log("設定ボタンクリック → 設定画面へ遷移！");
   };
 
