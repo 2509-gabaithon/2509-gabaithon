5465e0eee193c2bb7c85b51db034043e4b50a3c9
salieri256 <nakakou201250@gmail.com>
2025-09-07 16:20:47 +0900

    add: プレビュー用の画面を作った

--- src/app/view_stamp/page.tsx
+++ src/app/view_stamp/page.tsx
@@ -1,5 +1,25 @@
+"use client"
+
+import { StampRallyScreen } from "@/components/QuestScreen"
+import type { TabType } from '@/components/BottomTabNavigation';
+
 export default function ViewStamp() {
+  const handleBack = () => {
+    console.log("handleBack")
+  }
+
+  const handleSelectQuest = (quest: object) => {
+    console.log(`handleSelectOnsen %o`, quest)
+  }
+
+  const handleTabChange = (tabType: TabType) => {
+    console.log(`handleTabChange: ${tabType}`)
+  }
+  
   return (
-    <>保有スタンプ確認画面</>
+    <StampRallyScreen
+      onBack={handleBack}
+      onSelectQuest={handleSelectQuest}
+      onTabChange={handleTabChange} />    
   )
 }
