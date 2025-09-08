f0023ad4b68531f90e70b7dfa157273388821e4c
58140-web <s58140@ga.ariake-nct.ac.jp>
2025-09-08 16:48:57 +0900

    feat: layout.tsxを修正し、タイトル画面含む全体に「Kosugi Maru」フォントを適用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Kosugi_Maru } from "next/font/google"; // 追加
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const kosugi = Kosugi_Maru({
+  weight: "400", 
+  subsets: ["latin"], 
+  variable: "--font-kosugi-maru", 
 });
 
 export const metadata: Metadata = {
@@ -24,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={kosugi.className}>
         {children}
       </body>
     </html>
